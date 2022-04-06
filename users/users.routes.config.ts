f05d8fcb28eaf996943e307e86a49870edca257f
Tidbits JS <tidbitsjs@gmail.com>
2022-04-06 23:31:02 +0530

    DTO objects

--- users/users.routes.config.ts
+++ users/users.routes.config.ts
@@ -7,6 +7,39 @@ export class UsersRoutes extends CommonRoutesConfig {
   }
 
   configureRoutes() {
+    this.app
+      .route(`/users`)
+      .get((req: express.Request, res: express.Response) => {
+        res.status(200).send(`List of users`);
+      })
+      .post((req: express.Request, res: express.Response) => {
+        res.status(200).send(`Post to users`);
+      });
+
+    this.app
+      .route(`/users/:userId`)
+      .all(
+        (
+          req: express.Request,
+          res: express.Response,
+          next: express.NextFunction
+        ) => {
+          next();
+        }
+      )
+      .get((req: express.Request, res: express.Response) => {
+        res.status(200).send(`GET requested for id ${req.params.userId}`);
+      })
+      .put((req: express.Request, res: express.Response) => {
+        res.status(200).send(`PUT requested for id ${req.params.userId}`);
+      })
+      .patch((req: express.Request, res: express.Response) => {
+        res.status(200).send(`PATCH requested for id ${req.params.userId}`);
+      })
+      .delete((req: express.Request, res: express.Response) => {
+        res.status(200).send(`DELETE requested for id ${req.params.userId}`);
+      });
+
     return this.app;
   }
 }
