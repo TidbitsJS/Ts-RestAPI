e418e4a37ec139f8e25cc9495ef1595c018ceae9
Tidbits JS <tidbitsjs@gmail.com>
2022-04-07 23:40:35 +0530

    user dao

---
+++ users/daos/users.dao.ts
@@ -0,0 +1,93 @@
+import shortid from "shortid";
+import debug from "debug";
+
+import { CreateUserDto } from "../dto/create.user.dto";
+import { PatchUserDto } from "../dto/patch.user.dto";
+import { PutUserDto } from "../dto/put.user.dto";
+
+const log: debug.IDebugger = debug("app:in-memory-dao");
+
+/**
+ * NEVER USER THIS CLASS IN REAL LIFE.
+ * This class was created to ease the explanation of other topics in the corresponding article.
+ * For any real-life scenario, consider using an ODM/ORM to manage your own database in a better way.
+ */
+
+class UsersDao {
+  users: Array<CreateUserDto> = [];
+
+  constructor() {
+    log("Created new instance of UsersDao");
+  }
+
+  async addUser(user: CreateUserDto) {
+    user.id = shortid.generate();
+    this.users.push(user);
+    return user.id;
+  }
+
+  async getUsers() {
+    return this.users;
+  }
+
+  async getUserById(userId: string) {
+    return this.users.find((user: { id: string }) => user.id === userId);
+  }
+
+  async putUserById(userId: string, user: PutUserDto) {
+    const objIndex = this.users.findIndex(
+      (obj: { id: string }) => obj.id === userId
+    );
+
+    this.users.splice(objIndex, 1, user);
+    return `${user.id} updated via put`;
+  }
+
+  async patchUserById(userId: string, user: PatchUserDto) {
+    const objIndex = this.users.findIndex(
+      (obj: { id: string }) => obj.id === userId
+    );
+
+    let currentUser = this.users[objIndex];
+    const allowedPatchFields = [
+      "password",
+      "firstName",
+      "lastName",
+      "permissionLevel",
+    ];
+
+    for (let field of allowedPatchFields) {
+      if (field in user) {
+        // @ts-ignore
+        currentUser[field] = user[field];
+      }
+    }
+
+    this.users.splice(objIndex, 1, currentUser);
+    return `${user.id} patched`;
+  }
+
+  async removeUserById(userId: string) {
+    const objIndex = this.users.findIndex(
+      (obj: { id: string }) => obj.id === userId
+    );
+
+    this.users.splice(objIndex, 1);
+    return `${userId} removed`;
+  }
+
+  async getUserByEmail(email: string) {
+    const objIndex = this.users.findIndex(
+      (obj: { email: string }) => obj.email === email
+    );
+
+    let currentUser = this.users[objIndex];
+    if (currentUser) {
+      return currentUser;
+    } else {
+      return null;
+    }
+  }
+}
+
+export default new UsersDao();
